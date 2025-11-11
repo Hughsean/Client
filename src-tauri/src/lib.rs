ec192363de1dfd6693fcb3b8223d5a56c8d6d8ea
Hughsean <Hughsean.F@outlook.com>
2025-11-12 02:45:30 +0800

    添加 Tauri 应用构建工作流，更新 .gitignore 文件以包含 target 目录，优化 Cargo.toml 配置以提高发布版本性能，修改 tauri.conf.json 以调整窗口属性和启用打包功能

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,23 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let main_window = app.get_webview_window("main").unwrap();
+            
+            // 延迟显示窗口，等待内容加载
+            std::thread::spawn(move || {
+                std::thread::sleep(std::time::Duration::from_millis(400));
+                main_window.show().unwrap();
+                main_window.set_focus().unwrap();
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
